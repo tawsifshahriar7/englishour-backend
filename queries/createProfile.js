cbdd4811beabe59e9eee5536467af6a8773e2d00
tawsifshahriar7 <tawsifshahriar7@gmail.com>
2022-07-16 11:47:48 +0600

    Upto letterchange fetching

---
+++ queries/createProfile.js
@@ -0,0 +1,26 @@
+const { json } = require("express/lib/response");
+const status_codes = require("../utils/status_code/status_code");
+const Profile = require("../model/profile");
+
+const createProfile = async (req, res) => {
+  await Profile.create({
+    username: req.body.username,
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
+    dateofBirth: req.body.dateofBirth,
+    institution: req.body.institution,
+    class: req.body.class,
+    profile_picture: req.body.profile_picture,
+    current_level: req.body.current_level,
+  })
+    .then((result) => {
+      console.log(result);
+      return res.status(status_codes.SUCCESS).send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(status_codes.ERROR).send(err);
+    });
+};
+
+module.exports = createProfile;
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 const userInfo = require("../queries/user");
 const userRegister = require("../queries/userRegister");
 const userLogin = require("../queries/userLogin");
+const createProfile = require("../queries/createProfile");
+const profileInfo = require("../queries/userProfile");
+const letterchange = require("../queries/letterchange");
 
-router.get("/user", (req, res) => {
+router.get("/user/info", auth, (req, res) => {
   userInfo(req, res);
 });
 router.post("/user/register", (req, res) => {
@@ -12,5 +16,14 @@ router.post("/user/register", (req, res) => {
 });
 router.post("/user/login", (req, res) => {
   userLogin(req, res);
+});
+router.post("/user/createprofile", (req, res) => {
+  createProfile(req, res);
+});
+router.get("/user/profile", (req, res) => {
+  profileInfo(req, res);
+});
+router.get("/user/letterchange", (req, res) => {
+  letterchange(req, res);
 });
 module.exports = router;
